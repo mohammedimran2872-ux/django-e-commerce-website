321a70fdbd4fabafa0e8edee9f439dcaeeed3250
mohammed imran <mohammedimran2872@gmail.com>
2026-03-12 22:43:23 +0530

    Fix static files for deployment

--- game_website/settings.py
+++ game_website/settings.py
@@ -126,8 +126,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS =[STATIC_DIR]
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
